77893012baaacf04a63a0917aef6b2c333765ded
idobetesh <idobetesh@gmail.com>
2021-05-16 21:57:02 +0300

    address supported

---
+++ Ex1-Lexical-Analyzer/scanner.h
@@ -0,0 +1,14 @@
+//Define token types
+#define NUMBER 1
+#define FLOAT 2
+#define EMAIL 3
+#define PHONE 4
+#define ADDRESS 5
+#define COLON 6
+#define IDENTIFIER 7
+#define INTEGER 8
+#define OP 9
+#define FLOATING 10
+#define EMAIL_ADD 11
+#define PHONE_NUM 12
+#define HOME_ADDRESS 13
